da7c2606a66bd9146971c745e5e24cde502f9ccc
Juan17Informatico <juancampuzano2356@gmail.com>
2024-03-20 20:07:10 -0500

    Utilizar el operador spread '...' para desestructurar el arreglo y utilizar el setCategories para insertar mas valores al state de nuestro componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,19 +3,25 @@ import { useState } from "react";
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
+    const onAddCategory = () => {
+
+        //Categories
+        // console.log('Valorant');
+        setCategories( [ 'Valorant' ,...categories ] );
+        // setCategories( cat => [...cat, 'Valorant']); 
+    }
+
     console.log(categories);
     return (
         <>
             {/* Titulo */}
             <h1>GifExpertApp</h1>
             {/* Listado de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map((category) => {
-                  return <li key={ category }>{category}</li>; 
+                  return <li key={ category }>{category}</li>
                 })}
-                <li>ABC</li>
-                <li>XYZ</li>
-                <li>123</li>
             </ol>
             {/* Gif Item */}
         </>

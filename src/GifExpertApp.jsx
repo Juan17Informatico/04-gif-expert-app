6c3c9e3d19aab4f2bc458089e3a22be4a151f3e7
JuanCM <juancampuzano2356@gmail.com>
2024-03-21 16:57:28 -0500

    Se realizo la creacion del componente para agregar una nueva categoria y se inicio el componente 'GifGrid' para mandar a traer los gifs con los que se van a trabajar

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
-    const onAddCategory = () => {
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
 
         //Categories
-        // console.log('Valorant');
-        setCategories( [ 'Valorant' ,...categories ] );
-        // setCategories( cat => [...cat, 'Valorant']); 
-    }
+        setCategories([newCategory, ...categories]);
+        // setCategories( cat => [...cat, 'Valorant']);
+    };
 
-    console.log(categories);
     return (
         <>
             {/* Titulo */}
             <h1>GifExpertApp</h1>
-            {/* Listado de Gif */}
-            <button onClick={onAddCategory}>Agregar</button>
-            <ol>
-                {categories.map((category) => {
-                  return <li key={ category }>{category}</li>
-                })}
-            </ol>
-            {/* Gif Item */}
+
+            {/* input */}
+            <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+        
+            {
+                categories.map((category) => (
+                <GifGrid key={category} category={category} />
+                ))
+            }
         </>
     );
 };

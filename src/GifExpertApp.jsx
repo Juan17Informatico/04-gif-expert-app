3c197faa6b306326f6bf7dcae29c9b0c1532dbb8
JuanCM <juancampuzano2356@gmail.com>
2024-04-03 14:10:37 -0500

    Fin sección 8

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,22 +4,19 @@ import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(["One Punch"]);
-
     const onAddCategory = (newCategory) => {
         if (categories.includes(newCategory)) return;
         setCategories([newCategory, ...categories]);
-
     };
-
     return (
         <>
             <h1>GifExpertApp</h1>
-
             <AddCategory onNewCategory={(value) => onAddCategory(value)} />
-
-            {categories.map((category) => (
-                <GifGrid key={category} category={category} />
-            ))}
+            {
+                categories.map((category) => (
+                    <GifGrid key={category} category={category} />
+                ))
+            }
         </>
     );
 };
--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs");
 
 describe('Pruebas en <GifGrid />', () => { 
 
     const category = 'One punch';
 
     test('debe de mostrar el loading inicialmente', () => { 
+
+        useFetchGifs.mockReturnValue({
+            images: [], 
+            isLoading: true
+        }); 
 
         render(<GifGrid category={category} />);
         
@@ -14,6 +22,31 @@ describe('Pruebas en <GifGrid />', () => {
 
      });
 
-     test('debe de mostrar imagenes', () => {  }); 
+     test('debe de mostrar items cuando se cargan las imáganes useFetchGifs', () => { 
+
+        const gifs = [
+            {
+                id: "ABC", 
+                title: "Saitama",
+                url: "https://localhost/saitama.jpg"
+            }
+            ,{
+                id: "123", 
+                title: "Goku",
+                url: "https://localhost/goku.jpg"
+            }
+        ]; 
+
+        useFetchGifs.mockReturnValue({
+            images: gifs, 
+            isLoading: true
+        }); 
+        
+        render(<GifGrid category={category}/>);
+
+        expect( screen.getAllByRole('img').length ).toBe(2); 
+
+
+    }); 
 
  })
